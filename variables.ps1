7f88bc308c6d53ac2b1bf65a0e803c3f4ce197f2
Fernando Alfredo Sancho Samata <fernandosanchosamata@gmail.com>
2026-03-23 18:10:56 -0500

    my subcription hardcoded and sky alloed

--- variables.ps1
+++ variables.ps1
@@ -3,13 +3,13 @@ $LOC = "eastus"
 
 # AKS
 $AKS_NAME = "aks-cardops-demo"
-$AKS_NODE_SIZE = "Standard_D2s_v3"
+$AKS_NODE_SIZE = "Standard_B2s"
 $AKS_NODE_COUNT = 1
 
 # APIM
 $APIM_NAME = "apim-cardops-demo"
 
-$SUBSCRIPTION_ID = "4a42d9ba-d900-40bd-93ea-6e3548f68daa"
+$SUBSCRIPTION_ID = "23c7ebcf-b46c-4add-80d8-dd720ec68c4d"
 
 # COSMOS (Mongo API)
 $COSMOS_ACCOUNT = "cosmos-cardops-demo"   # unico en Azure
